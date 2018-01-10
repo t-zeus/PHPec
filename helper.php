9346a034d1748423e90295600610479d53568991
Tim <tim8670@gmail.com>
2018-01-10 20:53:53 +0800

    fixed:BaseControl -> _before param $ctx miss

--- helper.php
+++ helper.php
@@ -48,12 +48,12 @@ class BaseControl {
 	function __construct($ctx){
 		$this->ctx = $ctx;
 		if(method_exists($this, '_before')){
-			$this -> _before();
+			$this -> _before($this->ctx);
 		}
 	}
 	function __destruct(){
 		if(method_exists($this, '_after')){
-			$this -> _after();
+			$this -> _after($this->ctx);
 		}
 	}
 }
