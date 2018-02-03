421c9aae53fd4433cc729fe8f0afbd70d1780d57
Tim <tim8670@gmail.com>
2018-02-03 21:20:33 +0800

    update

--- src/interface.php
+++ src/interface.php
@@ -4,10 +4,10 @@ namespace PHPec;
 
 //Middleware接口
 interface Middleware{
-	public function begin($ctx);
+    public function begin($ctx);
     public function end($ctx);
 }
 //LogWriter
 interface LogWriter{
-	public function write($msg,$type);
+    public function write($msg,$type);
 }
