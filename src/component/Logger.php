ab7dd3ff6c7e94e62282aa4417eaad1654ec5829
TIM <huangtc@ucweb.com>
2018-03-22 18:58:25 +0800

    add: 日志级别控制

--- src/component/Logger.php
+++ src/component/Logger.php
@@ -5,12 +5,31 @@ class Logger implements \Psr\Log\LoggerInterface
 {
     use \PHPec\DITrait;
     use \Psr\Log\LoggerTrait;
+
+    private $levels = [
+        'debug'     => 1,
+        'info'      => 2,
+        'notice'    => 4,
+        'warning'   => 8,
+        'error'     => 16,
+        'critical'  => 32,
+        'alert'     => 64,
+        'emergency' => 128
+    ];
+
+    //private $all = 1 << 8 - 1;
      
     public function log($level, $message, array $context = array())
     {
-        $path = $this -> Config -> get('log_path', APP_PATH.'/../runtime/log');
-        $file = $level."_".date('ymd');
-        $msg  = sprintf("%s %s\n", date('H:i:s'), $message);
-        file_put_contents("$path/$file", $msg, FILE_APPEND);
+        if (!isset($this -> levels[$level])) { //throw exception
+            throw new \Psr\Log\InvalidArgumentException();
+        } 
+        $log_level = $this -> Config -> get('log_level', 1 << 8 -1);
+        if ($this -> levels[$level] & $log_level) {
+            $path = $this -> Config -> get('log_path', APP_PATH.'/../runtime/log');
+            $file = $level."_".date('ymd');
+            $msg  = sprintf("%s %s\n", date('H:i:s'), $message);
+            file_put_contents("$path/$file", $msg, FILE_APPEND);
+        }
     }
 }
