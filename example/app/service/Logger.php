31131e0bb7b08d37cea50b1e2325f0652b2c701f
Tim <tim8670@gmail.com>
2018-03-29 20:42:24 +0800

    modify Logger service example

--- example/app/service/Logger.php
+++ example/app/service/Logger.php
@@ -2,14 +2,14 @@
 namespace myapp\service;
 
 
-//演示如何编写服务, 需 implements \Psr\Log\LoggerInterface
-class Logger implements \Psr\Log\LoggerInterface
+//演示如何编写服务, Logger因为框架内也需要使用，所以需实现\PHPec\interfaces\Logger接口，以保持一致
+class Loggers implements \PHPec\interfaces\Logger
 {
     //添加\PHPec\DITrait,可使用自动依赖注入
     use \PHPec\DITrait;
-    use \Psr\Log\LoggerTrait;
+    use \PHPec\LoggerTrait;
      
-    public function log($level, $message, array $context = array())
+    public function log($level, $msg, ...$args)
     {
         //具体业务处理，比如按不同的level将log发送到不同的目标
     }
